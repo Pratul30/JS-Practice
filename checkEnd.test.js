0ccbe79dbd09b1fd3a689fb4e88c3a3758814972
Pratul30 <pratul.rocks7777@gmail.com>
2023-01-15 12:20:42 +0530

    feat: test for both countVowels functions

--- checkEnd.test.js
+++ checkEnd.test.js
@@ -1,9 +1,9 @@
 const funObj = require('./checkEnd')
 
 test('check if input ends with "Script"', ()=>{
-    expect(funObj.isEndScriptReg('JavaScript')).toBe(true)
+    expect(funObj.isEndsWithScriptReg('JavaScript')).toBe(true)
 })
 
 test('check if input ends with "Script"', ()=>{
-    expect(funObj.isEndScriptArrow('Javascript')).toBe(false)
+    expect(funObj.isEndsWithScriptArrow('Javascript')).toBe(false)
 })
